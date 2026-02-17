6e291b1f7eb2f99114186af8c946ec59dc6a108a
luanli <3352760396@qq.com>
2026-02-17 21:05:59 +0100

    bug7: buildScoreTable reverse access ancestors order ledgerAncestors[numAncestors - 1 - i] => i+1

--- src/xrpld/app/misc/NegativeUNLVote.cpp
+++ src/xrpld/app/misc/NegativeUNLVote.cpp
@@ -204,7 +204,7 @@ NegativeUNLVote::buildScoreTable(
     for (int i = 0; i < FLAG_LEDGER_INTERVAL; ++i)
     {
         for (auto const& v : validations.getTrustedForLedger(
-                 ledgerAncestors[numAncestors - 1 - i], seq - 2 - i))
+                 ledgerAncestors[i+1], seq - 2 - i))
         {
             if (scoreTable.count(v->getNodeID()))
                 ++scoreTable[v->getNodeID()];
